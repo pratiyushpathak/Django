70fdc725e60edaa5a027b063d201897df337641a
Pratiyush Pathak <56939096+pratiyushpathak@users.noreply.github.com>
2024-03-07 10:11:16 +0530

    Delete MySqlDB directory

--- MySqlDB/medicines/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Medicines  # Corrected import
-
-class MedicineForm(forms.ModelForm):
-    class Meta:
-        model = Medicines
-        fields = '__all__'
